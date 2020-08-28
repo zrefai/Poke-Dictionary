dca8052b2caf39858d823dab567f5cf62b513eda
Zaki <zkrefai@gmail.com>
2020-08-28 00:27:50 -0500

    Allow change in font

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useFonts } from "expo-font";
+import { AppLoading } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// import { AppFontLoader } from "./src/utils/AppFontLoader";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Pokemon_GB: require("./assets/fonts/PokemonGb-RAeo.ttf"),
+    Unown: require("./assets/fonts/PokemonUnownGb-YAWa.ttf"),
+  });
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Unown" }}>Hello Sammy Najib</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
