d40eb2bf469240e4ecd0b7e2c1a8b19b9f16d14c
Zaki <zkrefai@gmail.com>
2020-09-11 14:59:35 -0500

    Remove comments and add capitalize to errorLogging

--- src/utils/errorLog.js
+++ src/utils/errorLog.js
@@ -1,29 +1,36 @@
+import { capitalize } from "./capitalize";
 export function errorLog(error, setError, name = "", masterListFlag = 0) {
   if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
     console.log(
       "The request was made and the server responded with a status code"
     );
     console.log(
-      `${masterListFlag ? "ERROR @MASTER_LIST" : `ERROR @POKEMON_${name}`}`
+      `${
+        masterListFlag
+          ? "ERROR @MASTER_LIST"
+          : `ERROR @POKEMON_${captialize(name)}`
+      }`
     );
     console.log(error.response.status);
     setError(error.response.status);
   } else if (error.request) {
-    // The request was made but no response was received
-    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-    // http.ClientRequest in node.js
     console.log("The request was made but no response was received");
     console.log(
-      `${masterListFlag ? "ERROR @MASTER_LIST" : `ERROR @POKEMON_${name}`}`
+      `${
+        masterListFlag
+          ? "ERROR @MASTER_LIST"
+          : `ERROR @POKEMON_${captialize(name)}`
+      }`
     );
     console.log(error.request);
   } else {
-    // Something happened in setting up the request that triggered an Error
     console.log("Something happened in setting up the request");
     console.log(
-      `${masterListFlag ? "ERROR @MASTER_LIST" : `ERROR @POKEMON_${name}`}`
+      `${
+        masterListFlag
+          ? "ERROR @MASTER_LIST"
+          : `ERROR @POKEMON_${captialize(name)}`
+      }`
     );
     console.log("Error", error.message);
   }
